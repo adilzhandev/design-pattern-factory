fc9eff60ecf6031ccdb53adc5f3e5303ed1c1ac2
adilzhandev <251581@astanait.edu.kz>
2026-09-20 15:39:33 +0500

    Rename macOSFactory to MacOSFactory, remove temporary test class

--- src/logistics/Main.java
+++
@@ -1,8 +0,0 @@
-package logistics;
-
-public class Main {
-    public static void main(String[] args){
-        Logistics logistics = new RoadLogistics();
-        logistics.planDelivery("Sport equipment","Almaty");
-    }
-}
