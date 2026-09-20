2a01fadd4f742f22ac6e31247524ec12da7fcd84
adilzhandev <251581@astanait.edu.kz>
2026-09-20 14:37:41 +0500

    Add Main

---
+++ src/logistics/Main.java
@@ -0,0 +1,8 @@
+package logistics;
+
+public class Main {
+    public static void main(String[] args){
+        Logistics logistics = new RoadLogistics();
+        logistics.planDelivery("Sport equipment","Almaty");
+    }
+}
